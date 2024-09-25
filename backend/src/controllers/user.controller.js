70cc8c7c8203678855609ef563149e21767760dc
vinay-7808 <vinay211113233@gmail.com>
2024-09-25 22:27:24 +0530

    same site cookie

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -146,7 +146,8 @@ const logoutUser = asyncHandler( async (req, res) => {
     )
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: 'None'
     } 
     return res
     .status(200)
--- backend/src/middlewares/Auth.middleware.js
+++ backend/src/middlewares/Auth.middleware.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token = req.cookies?.accessToken
+        const token = req.cookies?.refreshToken
         if(!token) {
             throw new ApiError(401, "Unauthorized Request")
         }
