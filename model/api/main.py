c588d0175f1e262f3cc3c2b4684eeb0e3e13f468
Vinay Srivastava <vinay211113233@gmail.com>
2024-09-26 01:48:55 +0530

    Update main.py

--- model/api/main.py
+++ model/api/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://algo-solution.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
