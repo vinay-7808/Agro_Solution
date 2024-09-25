3def7773a0eef0cb79168b8cfd22e4b7edbee496
Vinay Srivastava <vinay211113233@gmail.com>
2024-09-25 23:01:09 +0530

    Update user.route.js

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -15,7 +15,7 @@ router.route("/register").post(
     registerUser
 )
 router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
