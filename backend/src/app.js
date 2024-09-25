1b062a5660e0a95862162c4c24d926ed289a0043
vinay-7808 <vinay211113233@gmail.com>
2024-09-26 02:10:46 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,11 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: 'https://algo-solution-frontend.onrender.com',
-    credentials: true
-})) // middleware to allow requests from these origin
+    origin: 'https://algo-solution-frontend.onrender.com', // Frontend URL
+    credentials: true,  // Allow cookies and other credentials to be sent
+    methods: ['*'],  // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Specify allowed headers
+}));
 
 app.use(express.json({limit: "16kb"})); // middleware for accepting json of 16kb
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // middleware for parsing the url
